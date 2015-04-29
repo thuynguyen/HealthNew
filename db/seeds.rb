0ac363e4befd53d4c2de2420f82dc0d4903c3958
Thuy Nguyen <thuy.nguyen@freerangetechnology.com>
2015-04-29 16:36:03 +0700

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all
-Service.destroy_all
-User.create(:email => "admin@gmail.com", password: "1234qwer", password_confirmation: "1234qwer")
-services = {"Kham Thai" => 60, "Sieu Am Thuong" => 80, "Sieu Am Mau" => 150, "Tuyen Giap" => 80}
-services.keys.each do |name|
-	Service.create(name: name, price: services[name])
-end
+# Service.destroy_all
+User.create(:email => "bson@gmail.com", password: "1234qwer", password_confirmation: "1234qwer")
+# services = {"Kham Thai" => 60, "Sieu Am Thuong" => 80, "Sieu Am Mau" => 150, "Tuyen Giap" => 80}
+# services.keys.each do |name|
+# 	Service.create(name: name, price: services[name])
+# end
